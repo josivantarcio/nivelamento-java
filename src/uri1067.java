b40448418177b4645ee8c142b63bee901c391191
Josevan Oliveira <josivantarcio@gmail.com>
2020-12-02 07:47:12 -0300

    Update uri1067.java

--- src/uri1067.java
+++ src/uri1067.java
@@ -9,9 +9,10 @@ public class uri1067 {
 		int x = sc.nextInt();
 		
 		for (int i=1; i<=x; i++) {
-			if (i % 2 != 0) {
-				System.out.println(i);
-			}
+			if (x >= 1 && x <= 1000) {
+				if (i % 2 != 0) {
+					System.out.println(i);
+				}
 		}
 		
 		sc.close();
